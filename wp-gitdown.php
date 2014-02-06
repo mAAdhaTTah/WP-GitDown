af4b3b43c2cc1c88efe8b69f7a78d03efa2658ee
mAAdhaTTah <jamesorodig@gmail.com>
2014-02-06 08:57:59 -0500

    Add update_remote_repo function to push local repo when repo settings change

--- wp-gitdown.php
+++ wp-gitdown.php
@@ -9,6 +9,8 @@
 	Text Domain:
 	Domain Path:
 	@todo write better code documentation
+	@todo write check for git 1.7.5 minimum
+	@todo write readme that says 
  */
 
 class WordPress_Gitdown {
@@ -17,25 +19,25 @@ class WordPress_Gitdown {
    * We use the Markdown <-> HTML functions from WP-Markdown
    */
   static $required = 'wp-markdown';
-  
+
   /**
    * Version #
    **/
   static $version = '1.0';
-  
+
   /**
    * Holds the values to be used in the fields callbacks
    */
   private $options;
-  
+
   /**
    * The name of the repo dir
    */
   static $repo_dir = 'gitdown';
-  
+
   /**
    * get_repo_path function.
-   * 
+   *
    * @access public
    * @static
    * @return void
@@ -48,6 +50,19 @@ class WordPress_Gitdown {
   }
 
   /**
+   * get_git_obj function.
+   *
+   * @access public
+   * @static
+   * @return void
+   */
+  static function get_git_obj() {
+   $repo_path = self::get_repo_path();
+   $git = Git::open($repo_path);
+   return $git;
+  }
+
+  /**
    * __construct function.
    *
    * @access public
@@ -55,7 +70,7 @@ class WordPress_Gitdown {
    */
   public function __construct() {
     register_activation_hook(__FILE__,array(__CLASS__, 'install' ));
-    add_action( 'admin_notices', array( __CLASS__, 'display_message' ) ) ;
+    add_action( 'admin_notices', array( __CLASS__, 'display_message' ) );
     register_activation_hook(__FILE__,array(__CLASS__, 'uninstall' ));
     require_once(dirname(__FILE__) . '/lib/Git.php');
     add_action( 'admin_menu', array( $this, 'gitdown_page' ) );
@@ -93,11 +108,11 @@ class WordPress_Gitdown {
       // @todo: Write better exit message
       exit ('<b>Requires WP-Markdown.</b>');
     }
-  }  
-  
+  }
+
   /**
    * initiate_repo function.
-   * 
+   *
    * @access public
    * @static
    * @return void
@@ -127,11 +142,10 @@ class WordPress_Gitdown {
       $repo = Git::create($repo_path);
     }
   }
-  
-  
+
   /**
    * display_message function.
-   * 
+   *
    * @access public
    * @static
    * @return void
@@ -141,17 +155,17 @@ class WordPress_Gitdown {
       add_option( 'wp-gitdown', self::$version );
       $html = '<div class="updated">';
   			$html .= '<p>';
+  			  // @todo Write better message
   				$html .= 'Don\'t forget to add your GitHub creds!';
         $html .= '</p>';
   		$html .= '</div><!-- /.updated -->';
 	    echo $html;
     }
   }
-  
-  
+
   /**
    * uninstall function.
-   * 
+   *
    * @access public
    * @static
    * @return void
@@ -165,7 +179,7 @@ class WordPress_Gitdown {
   				$html .= 'Try deactivating the plugin again :(';
   			$html .= '</p>';
   		$html .= '</div><!-- /.updated -->';
-  
+
   		echo $html;
     }
   }
@@ -189,7 +203,8 @@ class WordPress_Gitdown {
    */
   public function gitdown_settings_page() {
     // Set class property
-    $this->options = get_option('gitdown_settings'); ?>
+    $this->options = get_option('gitdown_settings');
+    $this->update_remote_repo($this->options); ?>
     <div class="wrap">
       <h2>My Settings</h2>
       <form method="post" action="options.php">
@@ -261,6 +276,10 @@ class WordPress_Gitdown {
       // @todo hash password before sending to database
       $new_input['github_password'] = sanitize_text_field( $input['github_password'] );
     }
+    if( isset( $input['github_repo'] ) ) {
+      // do we have to do anything to this?
+      $new_input['github_repo'] = $input['github_repo'];// = sanitize_text_field( $input['github_repo'] );
+    }
 
     return $new_input;
   }
@@ -271,8 +290,7 @@ class WordPress_Gitdown {
    */
   public function gitcreds_section() {
     print('Insert help text.<br />');
-    $repo_path = self::get_repo_path();
-    $git = Git::open($repo_path);
+    $git = $this->get_git_obj();
     $git->clean();
     echo $git->status(true);
   }
@@ -302,10 +320,10 @@ class WordPress_Gitdown {
       isset( $this->options['github_password'] ) ? esc_attr( $this->options['github_password']) : ''
     );
   }
-  
+
   /**
    * github_repo function.
-   * 
+   *
    * @access public
    * @return void
    */
@@ -316,25 +334,65 @@ class WordPress_Gitdown {
     );
   }
 
+  /**
+   * update_remote_repo function.
+   *
+   * @access public
+   * @param mixed $repo
+   * @return void
+   */
+  public function update_remote_repo($repo) {
+    if ($repo == null) { return; }
+    // it's possible this function will receive null
+    if ( !isset($repo['github_username'], $repo['github_password'], $repo['github_repo'] ) ) { return; }
+    $git = self::get_git_obj();
+
+    // @todo strip 'https://'
+
+    // check if anything changed
+    // note this command req git 1.7.5
+    if ( $repo['github_repo'] !== $git->run( "ls-remote --get-url origin" ) ) {
+      // check if a remote is defined
+      if ( $git->run( "ls-remote --get-url origin" ) !== 'origin' ) {
+        // delete current remote repo
+        $git->run("remote rm origin");
+      }
+
+      $repo_url = 'https://' . $repo['github_username'] . ':' . $repo['github_password'] . '@' . $repo['github_repo'];
+      // add new remote repo
+      $git->run("remote add origin " . $repo_url);
+
+      // publish current repo use git push -u origin master so we can just do
+      // git push/pull; requires git 1.7.0
+      // @todo check for errors
+      $git->run("push -u origin master");
+    }
+  }
+
+  /**
+   * export_all_button function.
+   *
+   * @access public
+   * @return void
+   */
   public function export_all_button() {
     // @todo make only this fire when clicked, instead of firing when DB updated
     // may need to unhook the action
     echo '<input type="submit" id="export_all" name="export_all" class="button button-secondary" value="Export All Posts" />';
   }
-  
+
   /**
    * export_all function.
-   * 
+   *
    * @access public
    * @return void
    */
   public function export_all() {
     if (isset($_POST['export_all'])) {
       // initialize the git object
-      $repo_path = self::get_repo_path();
-      $git = Git::open($repo_path);
+      $git = self::get_git_obj();
       $git->clean();
-      
+
       // get all posts
       $query_args = array( 'post_type' => 'post',
                            'orderby'   => 'post_date'
@@ -342,35 +400,35 @@ class WordPress_Gitdown {
       $all_posts = get_posts($query_args);
 
       foreach ( $all_posts as $post ) {
-        
+
     		// Convert HTML content to Markdown
     		$html_content = $post->post_content;
     		$markdown_content = wpmarkdown_html_to_markdown($html_content);
-    		
+
     		// get slug + ID
     		$slug = $post->post_name;
     		$post_id = $post->ID;
-    		
+
     		// concatenate filename
     		$filename = $post_id . '-' . $slug . '.md';
-    		
+
     		// Export that Markdown to a .md file in $repo_path
     		// @todo rewrite this file creation function with WP_Filesystem API
-    		file_put_contents($repo_path . '/' . $filename, $markdown_content);
-    		
+    		file_put_contents(self::get_repo_path() . '/' . $filename, $markdown_content);
+
     		// Stage new file
     		$git->add($filename);
-    		
-    		// Commit 
+
+    		// Commit
     		// @todo need to react properly to git Exception where 'who you are' not set
     		if ($git->status() !== "# On branch master nothing to commit (working directory clean)") {
       		$message = 'Result of Export All Posts: exported ' . $post->post_title;
-      		$git->commit($message);	
+      		$git->commit($message);
     		}
     	}
       // Restore original Post Data
       wp_reset_postdata();
-      
+
       // push to origin:master
       // need to check if gitcreds set properly for this to run
     }
